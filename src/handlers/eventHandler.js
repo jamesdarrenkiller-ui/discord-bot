3064afade0ebf59a68482490607d5896644f8565
James <james.darren.killer@gmail.com>
2026-08-15 21:30:34 +0530

    refactor(events): wire modular security welcome automod and leveling systems

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,45 +1,30 @@
-const { PermissionsBitField } = require('discord.js');
-const { getGuild, save } = require('../utils/database');
+const { handleMessage } = require('../systems/automod');
+const { awardMessageXp } = require('../systems/leveling');
+const { memberJoined, memberLeft } = require('../systems/welcome');
+const { checkMemberAdd } = require('../systems/security');
 const { log } = require('../utils/logger');
 
 function registerEvents(client) {
   client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    client.user.setActivity('/help | Multipurpose Bot');
+    client.user.setActivity('/help | All-in-One Bot');
   });
 
   client.on('guildMemberAdd', async member => {
-    const cfg = getGuild(member.guild.id);
-    if (cfg.welcomeChannel) {
-      const channel = member.guild.channels.cache.get(cfg.welcomeChannel);
-      if (channel) {
-        const message = cfg.welcomeMessage
-          .replaceAll('{user}', `<@${member.id}>`)
-          .replaceAll('{server}', member.guild.name);
-        await channel.send(message).catch(() => {});
-      }
-    }
-    save();
+    await memberJoined(member);
+    await checkMemberAdd(member);
     await log(member.guild, 'Member Joined', `${member.user.tag} joined the server.`);
   });
 
-  client.on('guildMemberRemove', member => {
-    log(member.guild, 'Member Left', `${member.user?.tag || member.id} left the server.`);
+  client.on('guildMemberRemove', async member => {
+    await memberLeft(member);
+    await log(member.guild, 'Member Left', `${member.user?.tag || member.id} left the server.`);
   });
 
   client.on('messageCreate', async message => {
     if (message.author.bot || !message.guild) return;
-    const cfg = getGuild(message.guild.id);
-    if (!cfg.automod) return;
-
-    const blocked = ['discord.gg/', 'free nitro', 'free-nitro'];
-    if (blocked.some(word => message.content.toLowerCase().includes(word)) &&
-        !message.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
-      await message.delete().catch(() => {});
-      const warning = await message.channel.send(`⚠️ ${message.author}, that message was removed by AutoMod.`).catch(() => null);
-      if (warning) setTimeout(() => warning.delete().catch(() => {}), 5000);
-      await log(message.guild, 'AutoMod', `${message.author.tag}'s message was removed.`);
-    }
+    await handleMessage(message);
+    await awardMessageXp(message);
   });
 }
 
