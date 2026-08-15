f21de4d52b221476b561ab042471764b26b1ccab
James <james.darren.killer@gmail.com>
2026-08-15 22:55:51 +0530

    Set default prefix to question mark

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   token: process.env.DISCORD_TOKEN,
   clientId: process.env.CLIENT_ID,
   guildId: process.env.GUILD_ID || null,
-  prefix: process.env.PREFIX || '!',
+  prefix: process.env.PREFIX || '?',
   mongoUri: process.env.MONGODB_URI,
   aiApiKey: process.env.AI_API_KEY,
   aiBaseUrl: process.env.AI_BASE_URL || 'https://api.openai.com/v1',
