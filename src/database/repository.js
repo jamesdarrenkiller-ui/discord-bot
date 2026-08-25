e91cf7d7a94a70fd5b38abe932147c450b59af1f
James <james.darren.killer@gmail.com>
2026-08-25 18:58:28 +0530

    fix: scope repository user access by guild

--- src/database/repository.js
+++ src/database/repository.js
@@ -1,8 +1,31 @@
 const { Guild, User, Warning, Ticket, Giveaway } = require('./models');
 
-const getGuild = guildId => Guild.findOneAndUpdate({ guildId }, { $setOnInsert: { guildId } }, { upsert: true, new: true });
-const getUser = userId => User.findOneAndUpdate({ userId }, { $setOnInsert: { userId } }, { upsert: true, new: true });
-const addWarning = (guildId, userId, moderatorId, reason) => Warning.create({ guildId, userId, moderatorId, reason });
-const getWarnings = (guildId, userId) => Warning.find({ guildId, userId }).sort({ createdAt: -1 });
+const getGuild = guildId => Guild.findOneAndUpdate(
+  { guildId },
+  { $setOnInsert: { guildId } },
+  { upsert: true, new: true }
+);
 
-module.exports = { Guild, User, Warning, Ticket, Giveaway, getGuild, getUser, addWarning, getWarnings };
+const getUser = (guildId, userId) => User.findOneAndUpdate(
+  { guildId, userId },
+  { $setOnInsert: { guildId, userId } },
+  { upsert: true, new: true }
+);
+
+const addWarning = (guildId, userId, moderatorId, reason) =>
+  Warning.create({ guildId, userId, moderatorId, reason });
+
+const getWarnings = (guildId, userId) =>
+  Warning.find({ guildId, userId }).sort({ createdAt: -1 });
+
+module.exports = {
+  Guild,
+  User,
+  Warning,
+  Ticket,
+  Giveaway,
+  getGuild,
+  getUser,
+  addWarning,
+  getWarnings,
+};
